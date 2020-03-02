0c98f5ea0a3c9dedfc8ea2fbc01fb4181b6bc707
chenchen <756686879@qq.com>
2020-03-02 17:06:37 +0800

    FrostedMonkey

---
+++ src/main/java/threadobjectclasscommonmethods/Wait.java
@@ -0,0 +1,14 @@
+package threadobjectclasscommonmethods;
+
+/**
+ * @ClassName Wait
+ * @Author chenchen
+ * @Date 2020/3/2 17:05
+ * @Version 1.0
+ * 展示wait和notify的基本用法
+ * 1、研究代码执行顺序
+ * 2、证明wait释放锁
+ **/
+public class Wait {
+
+}
